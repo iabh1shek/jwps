216145801bd498490f0e9cdd21385f48dffee63c
iabh1shek <abhishek.aug122001@gmail.com>
2026-03-05 23:56:46 +0530

    f string

---
+++ pairing.py
@@ -0,0 +1,6 @@
+class  Pairing : 
+    first_number = int(input("enter first number ")) 
+    second_number= int(input("enter second number "))
+show_sum = Pairing() 
+#f string method 
+print(f"{show_sum.first_number} + {show_sum.second_number} = {show_sum.first_number + show_sum.second_number}")
