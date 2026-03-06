90e18cce04135ac59daf9657ab1d9097919954f1
iabh1shek <abhishek.aug122001@gmail.com>
2026-03-06 14:25:10 +0530

    eg

--- constructor.py
+++ constructor.py
@@ -5,6 +5,7 @@ class Car:
     def car_details(self, brand,model):
         self.brand = brand
         self.model = model
+        model =model 
         print(self.brand, self.model)
 car1 = Car()
 car1.car_details("BMW","M5")
