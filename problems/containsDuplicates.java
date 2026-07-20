99f2a57be546d5c38081edc0d9be2a7693a742c7
iabh1shek <abhishek.aug122001@gmail.com>
2026-07-20 21:44:46 +0530

    java concepts 1

--- problems/containsDuplicates.java
+++ problems/containsDuplicates.java
@@ -2,41 +2,41 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class containsDuplicates {
-    // brute force 
-    public boolean output(int[] array) { 
-        for(int i=0; i<array.length; i++){ 
-            boolean flag = false ; 
+    // brute force
+    public boolean output(int[] array) {
+        for(int i=0; i<array.length; i++){
+            
 
-            for(int j = i ; j< array.length; j++){ 
-                if(i != j) { 
-                    if(array[i] == array[j]){ 
-                        return true ; 
+            for(int j = i ; j< array.length; j++){
+                if(i != j) {
+                    if(array[i] == array[j]){
+                        return true ;
                     }
                 }
-                else{ 
-                    continue ; 
+                else{
+                    continue ;
                 }
             }
         }
 
-        return false; 
+        return false;
     }
 
 
-    // optimal solution 
-    
-    public boolean optimalSolution(int[] nums) { 
+    // optimal solution
 
-        Set<Integer> set = new HashSet<>(); 
-        for(int i=0; i<nums.length; i++){ 
+    public boolean optimalSolution(int[] nums) {
 
-            if(set.contains(nums[i])) { 
-                return true; 
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+
+            if(set.contains(nums[i])) {
+                return true;
             }
-            set.add(nums[i]); 
+            set.add(nums[i]);
 
 
         }
-        return false; 
+        return false;
     }
 }
---
+++ problems/validanagram.java
@@ -0,0 +1,9 @@
+public class validanagram {
+    public boolean output(String s, String t) { 
+        // using key ,value pair .. .store key as char and its occurences in every string .. 
+        
+        
+
+    
+    }
+}
